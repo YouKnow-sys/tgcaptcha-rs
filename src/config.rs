497ce3b133740b50f2b9d000ea764016344c88e9
YouKnow <saeid003424@gmail.com>
2023-09-01 03:17:29 +0430

    update the Config file with a complete example

--- src/config.rs
+++ src/config.rs
@@ -1,17 +1,21 @@
 use std::{collections::HashMap, time::Duration};
 
+use serde_with::{serde_as, DisplayFromStr};
 use config::{Config as PConfing, ConfigError, File, Environment};
 use serde::{Serialize, Deserialize};
 use teloxide::{types::{ChatId, UserId, User}, utils::html::user_mention_or_link};
 
 use crate::join_check::Question;
 
+#[serde_as]
 #[derive(Default, Serialize, Deserialize)]
 pub struct BotConfig {
     /// List of allowed group, if `None` bot will allow all groups
     pub allowed_groups: Option<Vec<ChatId>>,
+    #[serde(skip)]
     fallback_group_settings: GroupSettings,
-    groups_settings: HashMap<ChatId, GroupSettings>,
+    #[serde_as(as = "HashMap<DisplayFromStr, _>")]
+    groups_settings: HashMap<i64, GroupSettings>,
 }
 
 impl BotConfig {
@@ -29,7 +33,7 @@ impl BotConfig {
     }
 
     pub fn get(&self, chat_id: &ChatId) -> &GroupSettings {
-        match self.groups_settings.get(chat_id) {
+        match self.groups_settings.get(&chat_id.0) {
             Some(s) => s,
             None => &self.fallback_group_settings,
         }
